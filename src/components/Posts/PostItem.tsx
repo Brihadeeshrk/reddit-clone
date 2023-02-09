018883ffcd3190fdc81420376cf94f23f9f02b3d
Brihadeeshrk <hello@briha.live>
2023-02-09 14:04:07 +0530

    post delete ok"

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -1,10 +1,11 @@
 import { Post } from "@/src/atoms/postAtom";
-import { Flex, Icon, Image, Stack, Text } from "@chakra-ui/react";
+import { Flex, Icon, Image, Skeleton, Stack, Text } from "@chakra-ui/react";
 import moment from "moment";
 import React from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsChat, BsDot } from "react-icons/bs";
 import { FaReddit } from "react-icons/fa";
+import { useState } from "react";
 import {
   IoArrowDownCircleOutline,
   IoArrowDownCircleSharp,
@@ -21,7 +22,7 @@ type PostItemProps = {
   userVoteValue?: number;
   onVote: () => {};
   onSelectPost: () => void;
-  onDeletePost: () => {};
+  onDeletePost: (post: Post) => Promise<boolean>;
 };
 
 const PostItem: React.FC<PostItemProps> = ({
@@ -33,6 +34,22 @@ const PostItem: React.FC<PostItemProps> = ({
   onSelectPost,
   onDeletePost,
 }) => {
+  const [loadingImage, setLoadingImage] = useState(true);
+  const [error, setError] = useState("");
+
+  const handleDelete = async () => {
+    try {
+      const success = await onDeletePost(post);
+
+      if (!success) {
+        throw new Error("Failed to delete the post");
+      }
+      console.log("post was successfully deleted");
+    } catch (error: any) {
+      console.log("handleDelete Error", error.message);
+      setError(error.message);
+    }
+  };
   return (
     <Flex
       border="1px solid"
@@ -91,7 +108,16 @@ const PostItem: React.FC<PostItemProps> = ({
           <Text fontSize="10pt">{post.body}</Text>
           {post.imageURL && (
             <Flex justify="center" align="center" p={2}>
-              <Image src={post.imageURL} maxHeight="460px" alt="post image" />
+              {loadingImage && (
+                <Skeleton height="200px" width="100%" borderRadius={4} />
+              )}
+              <Image
+                src={post.imageURL}
+                maxHeight="460px"
+                alt="post image"
+                display={loadingImage ? "none" : "flex"}
+                onLoad={() => setLoadingImage(false)}
+              />
             </Flex>
           )}
         </Stack>
@@ -133,7 +159,7 @@ const PostItem: React.FC<PostItemProps> = ({
               borderRadius={4}
               _hover={{ bg: "gray.200" }}
               cursor="pointer"
-              onClick={onDeletePost}
+              onClick={handleDelete}
             >
               <Icon as={AiOutlineDelete} mr={2} />
               <Text fontSize="9pt">Delete</Text>
--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { postState } from "../atoms/postAtom";
+import { Post, postState } from "../atoms/postAtom";
+import { useState } from "react";
+import { deleteObject, ref } from "firebase/storage";
+import { firestore, storage } from "../firebase/clientApp";
+import { deleteDoc, doc } from "firebase/firestore";
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
+  const [error, setError] = useState("");
 
   const onVote = async () => {};
+
   const onSelectPost = () => {};
-  const onDeletePost = async () => {};
+
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      //if an image exixts, delete the image from firebase storage
+      if (post.imageURL) {
+        const imageRef = ref(storage, `posts/${post.id}/image`);
+        await deleteObject(imageRef);
+      }
+
+      //delete the document from firestore
+      const postDocRef = doc(firestore, "posts", post.id!);
+      await deleteDoc(postDocRef);
+
+      //update recoil state
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter((item) => item.id !== post.id),
+      }));
+      return true;
+    } catch (error: any) {
+      setError(error.message);
+      console.log("onDeletePost error", error.message);
+      return false;
+    }
+  };
+
   return {
     postStateValue,
     setPostStateValue,
